0d5737125607d5653d644f2740e0336e69c11d9e
Hubert Mandeville <hubert.mandeville@nytimes.com>
2015-09-04 16:17:44 -0400

    Fixed player.

--- code.js
+++ code.js
@@ -1,16 +1,45 @@
-var audio_state = "playing";
+/*
+ * http://developers.soundcloud.com/docs/api/html5-widget
+ *
+ * To Do
+ *  - Have playlist loop.
+ *  - Fix play/pause buttons on iphone.
+ *  - Make look good on iphone.
+ */
+var audio_state = "paused";
+var sc_widget = null;
 
 $(function() {
+    var sc_iframe = document.querySelector('#soundcloud_player_iframe');
+    sc_widget = SC.Widget(sc_iframe);
+
+    sc_widget.bind(SC.Widget.Events.READY, function() {
+        update_song_info();
+    });
+
 
     $("#button_play_pause").on("click", function() {
+        update_song_info();
+        // XXX: instead of state could check isPaused
         if (audio_state == "playing") {
-            $("#button_play_pause").html("&#9612;&#9612;");
-            audio_state = "paused";
-        } else {
             $("#button_play_pause").html("&#x25B6;");
+            audio_state = "paused";
+            sc_widget.pause();
+        } else {
+            $("#button_play_pause").html("&#9612;&#9612;");
             audio_state = "playing";
+            sc_widget.play();
         }
     });
+    $("#button_previous").on("click", function() {
+        sc_widget.prev();
+        update_song_info();
+    });
+    $("#button_next").on("click", function() {
+        sc_widget.next();
+        update_song_info();
+    });
+
 
     
     $(document).on( 'scroll', function(){
@@ -28,3 +57,23 @@ $(function() {
 });
 
 
+
+
+function update_song_info() {
+    sc_widget.getCurrentSound(function(sound) {
+        $("#song_title").html("<a href=\"" + sound.permalink + "\">" + sound.title + "</a>");
+
+        $("#nav").css("background-image", "linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.9) ), url('" + sound.waveform_url + "')");
+
+        /*
+        if (sound.artwork_url != null) {
+            $("#song_art").attr("src", sound.artwork_url);
+            $("#song_art").css("display", "inline-block");
+        } else {
+            $("#song_art").attr("src", "");
+            $("#song_art").css("display", "none");
+        }
+        */
+
+    });
+}
