5f9a2b928bd4312135c4ee82dc7346a51ab9cbff
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-09-07 23:18:01 -0400

    Updated

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 scp code.js  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/
 scp index.html  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/
 scp style.css  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/
-#scp images/*.png  escalona@medianoche.us:/home/escalona/medianoche.us.youngjimmy/images/
+#scp images/jaw-x-ray.jpg  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/images/
+#scp images/*.jpg  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/images/
+#scp images/*.png  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/images/
 #scp videos/*.mov  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/videos/
 
