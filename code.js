3c5c8bde3eea8882ed71f4b556692b5e4e2edcc7
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-09-10 00:29:12 -0400

    Added vimeo video.

--- code.js
+++ code.js
@@ -15,17 +15,31 @@ $(function() {
 
     rotate_images_timer = setInterval(rotate_images, 10000);
 
+    // the video is 1920x1080
+    var width = $(window).width();
+    var height = Math.ceil((width / 1920) * 1080);
+    $("#video_boyz").css("height", height);
+    $("#video_boyz").css("width", width);
+    $(window).resize(function() {
+        var width = $(window).width();
+        var height = Math.ceil((width / 1920) * 1080);
+        $("#video_boyz").css("height", height);
+        $("#video_boyz").css("width", width);
+    });
+    
     /* play or pause video depending on scroll position */
     $(document).on( 'scroll', function(){
         var boyz_top = $("#video_boyz").position().top;
         var boyz_bottom = boyz_top + $("#video_boyz").height();
         var document_top = $(document).scrollTop();
 
+        /*
         if ((document_top >= (boyz_top - 50)) && (document_top <= (boyz_bottom + 50))) {
             $("#video_boyz").get(0).play();
         } else {
             $("#video_boyz").get(0).pause();
         }
+        */
     });
 
     $("#button_xray").on("click", function() {
