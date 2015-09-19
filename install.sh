c64192d34a9eb20787a6d672b8d2081a7f77bae4
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-09-19 10:11:35 -0400

    Updated to point at godaddy.com

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-scp code.js  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/
-scp index.html  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/
-scp style.css  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/
-#scp images/jaw-x-ray.jpg  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/images/
-#scp images/*.jpg  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/images/
-#scp images/*.png  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/images/
-#scp videos/*.mov  escalona@medianoche.us:/home/escalona/medianoche.us/youngjimmy/videos/
+# Copy files to the web server (107.180.44.139).
+scp code.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+scp index.html youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+scp robots.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+scp sitemap.xml youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+scp images/*jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+scp images/*png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+
+
+
 
