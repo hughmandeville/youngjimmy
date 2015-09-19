ad58fd43c68ed91225ec73c63f16c2fcf2c2720e
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-09-19 19:25:03 -0400

    Updated.

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 # Copy files to the web server (107.180.44.139).
-scp code.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+#scp code.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 scp index.html youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
-scp robots.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
-scp sitemap.xml youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+#scp robots.txt youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+#scp sitemap.xml youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
-scp images/*jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
-scp images/*png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp fonts/* youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/fonts/
+#scp images/*jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/*png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/young_jimmy_og.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 
 
 
