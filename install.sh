f0eb5c227883d0f0944047f4b2340e4f5f3f187b
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-10-17 21:32:47 -0400

    Added itunes image.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Copy files to the web server (107.180.44.139).
-#scp code.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+scp code.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 scp index.html youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp robots.txt youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp sitemap.xml youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
@@ -9,6 +9,8 @@ scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp images/*png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/young_jimmy_og.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/young_jimmy_120x120.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/button_itunes_available.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+
 
 
 
