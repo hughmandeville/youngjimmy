422b0e24acf349b3831fa2e2589bb64319ab197d
Hugh Mandeville <hubert.mandeville@nytimes.com>
2016-02-26 22:36:16 -0500

    Added the movies images.

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp images/young_jimmy_og.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/young_jimmy_120x120.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/button_itunes_available.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/the_movies_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/the_movies_front_400x400.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 
 
 
