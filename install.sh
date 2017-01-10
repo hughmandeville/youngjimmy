27491d55fc4b1fae66ddb5b1c4852540234c2802
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2017-01-10 09:28:57 -0500

    added some images

--- install.sh
+++ install.sh
@@ -8,12 +8,21 @@ scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp fonts/* youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/fonts/
 #scp images/*jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/*png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/boys_in_the_hood_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/logo_itunes.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/logo_soundcloud.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/logo_youtube.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/young_jimmy_og.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/young_jimmy_120x120.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/button_itunes_available.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/the_movies_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/the_movies_front_400x400.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
-
+#scp images/uptown_gun_sounds_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/uptown_gun_sounds_400x400.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_aug_16.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_jan_7.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_jan_14.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp videos/yj.mp4 youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/videos/
 
 
 
