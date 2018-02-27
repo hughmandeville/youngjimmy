d976981bae442879c769e5d90859cd290ec99480
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2018-02-26 22:07:52 -0500

    added images

--- install.sh
+++ install.sh
@@ -1,20 +1,28 @@
 # Copy files to the web server (107.180.44.139).
-scp code.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+#scp code.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 scp index.html youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp robots.txt youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp sitemap.xml youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
-scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
+#scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp waveform.js youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp fonts/* youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/fonts/
 #scp images/*jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/*png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/boys_in_the_hood_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/hat_kap_ernick.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/hat_pink_panther.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/hat_supremacy.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/hat_yjdad.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/logo_itunes.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/logo_amazon.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/logo_soundcloud.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/logo_youtube.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/logo_yjjy_2.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/young_jimmy_og.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/young_jimmy_120x120.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/button_itunes_available.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/tha_black_punks_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/the_partys_over_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/the_movies_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/the_movies_front_400x400.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/uptown_gun_sounds_promo.png youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
@@ -22,6 +30,23 @@ scp style.css youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/
 #scp images/poster_show_aug_16.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/poster_show_jan_7.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
 #scp images/poster_show_jan_14.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_mar_18_2017.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_may_11_2017.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_sep_23_2017.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+
+scp images/promo_yellow_yellow.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+
+#scp images/album_cover_black_punks.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/album_back_black_punks.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/album_cover_partys_over.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/album_back_partys_over.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/album_cover_hood_rock.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/album_cover_hood_rock_2.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/album_back_hood_rock_2.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/album_cover_sankofa.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_aug_12_2017.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+#scp images/poster_show_aug_12_2017-after.jpg youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/images/
+
 #scp videos/yj.mp4 youngjimmy777@107.180.44.139:/home/youngjimmy777/public_html/videos/
 
 
