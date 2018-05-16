3cd943388357fe88140e144dbc53f0ff2af668ea
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2018-05-16 08:27:47 -0400

    added sob poster.

--- code2.js
+++ code2.js
@@ -32,27 +32,34 @@ function set_content() {
         page = "index";
     }
     if (page == "index") {
-        $("#content").html("<img class='big_img' src='images/promo_yellow_yellow.jpg'/><br/><br/><img class='big_img' src='images/yj_main_2_alone.jpg'/>");
+        $("#content").html(
+            "<a href='https://itunes.apple.com/album/id1375809976?ls=1&app=itunes'><img class='big_img' src='images/promo_yellow_yellow.jpg'/></a><br/><br/><br/><br/>" +
+            "<a href='https://www.ticketweb.com/event/late-to-the-party-yj-sobs-tickets/8361045?pl=sobs&edpPlParam=%3Fpl%3Dsobs'><img class='big_img' src='images/poster_show_jun_12_2018.jpg'></a><br/>");
+            // "<img class='big_img' src='images/yj_main_2_alone.jpg'/>"
     } else if (page == "merch") {
         $("#content").html("<div class='box'><img class='hat' src='images/hat_kap_ernick.png'></div>" +
                            "<div class='box'><img class='hat' src='images/hat_yjdad.png'></div>" +
                            "<div class='box'><img class='hat' src='images/hat_supremacy.png'></div>" +
                            "<div class='box'><img class='hat' src='images/hat_pink_panther.png'></div>");
     } else if (page == "music") {
-        $("#content").html("<div class='box'><a href='https://itunes.apple.com/us/album/the-partys-over-ep/1304105898'><img class='album_cover' data-cover='images/album_cover_partys_over.jpg' data-back='images/album_back_partys_over.jpg' src='images/album_cover_partys_over.jpg' alt='The Partys Over'/></a></div>" +
-                           "<div class='box'><a href='https://soundcloud.com/cheezcakekidzrecords/sets/tha-black-punks'><img class='album_cover' data-cover='images/album_cover_black_punks.jpg' data-back='images/album_back_black_punks.jpg' src='images/album_cover_black_punks.jpg' alt='Tha Black Punks'/></a></div>" +
-                           "<div class='box'><a href='https://soundcloud.com/cheezcakekidzrecords/sets/young-jimmy-hood-rock'><img class='album_cover' data-cover='images/album_cover_hood_rock.jpg' data-back='images/album_cover_hood_rock.jpg' src='images/album_cover_hood_rock.jpg' alt='Hood Rock'/></a></div>" +
-                           "<div class='box'><a href='images/album_back_hood_rock_2.jpg'><img class='album_cover' data-cover='images/album_cover_hood_rock_2.jpg' data-back='images/album_back_hood_rock_2.jpg' src='images/album_cover_hood_rock_2.jpg' alt='Hood Rock 2'/></a></div>" +
-                           "<div class='box'><a href='https://soundcloud.com/cheezcakekidzrecords/sets/to-my-chagrin'><img class='album_cover' data-cover='images/album_cover_sankofa.jpg' data-back='images/album_cover_sankofa.jpg' src='images/album_cover_sankofa.jpg' alt='Sankofa'/></a></div>"
+        $("#content").html(
+            "<div class='box'><a href='https://itunes.apple.com/album/id1375809976?ls=1&app=itunes'><img class='album_cover' src='images/album_cover_yy.jpg' data-cover='images/album_cover_yy.jpg' data-back='images/album_back_yy.jpg' alt='Yellow Yellow'/></a></div>" +
+            "<div class='box'><a href='https://itunes.apple.com/us/album/the-partys-over-ep/1304105898'><img class='album_cover' data-cover='images/album_cover_partys_over.jpg' data-back='images/album_back_partys_over.jpg' src='images/album_cover_partys_over.jpg' alt='The Partys Over'/></a></div>" +
+            "<div class='box'><a href='https://soundcloud.com/cheezcakekidzrecords/sets/tha-black-punks'><img class='album_cover' data-cover='images/album_cover_black_punks.jpg' data-back='images/album_back_black_punks.jpg' src='images/album_cover_black_punks.jpg' alt='Tha Black Punks'/></a></div>" +
+            "<div class='box'><a href='https://soundcloud.com/cheezcakekidzrecords/sets/young-jimmy-hood-rock'><img class='album_cover' data-cover='images/album_cover_hood_rock.jpg' data-back='images/album_cover_hood_rock.jpg' src='images/album_cover_hood_rock.jpg' alt='Hood Rock'/></a></div>" +
+            "<div class='box'><a href='images/album_back_hood_rock_2.jpg'><img class='album_cover' data-cover='images/album_cover_hood_rock_2.jpg' data-back='images/album_back_hood_rock_2.jpg' src='images/album_cover_hood_rock_2.jpg' alt='Hood Rock 2'/></a></div>" +
+            "<div class='box'><a href='https://soundcloud.com/cheezcakekidzrecords/sets/to-my-chagrin'><img class='album_cover' data-cover='images/album_cover_sankofa.jpg' data-back='images/album_cover_sankofa.jpg' src='images/album_cover_sankofa.jpg' alt='Sankofa'/></a></div>"
                           );
         $("#soundcloud_player_iframe").css("height", "400px");
         $("#soundcloud_player_iframe").css("position", "relative");
     } else if (page == "press") {
         $("#content").html("<a href='http://afropunk.com/2014/10/new-music-unreleased-sankofa-album-to-my-chagrin-featuring-young-jimmy/'><img class='big_img' src='images/yj_afropunk.jpg' alt='Afropunk'/></a>");
     } else if (page == "shows") {
-        $("#content").html("<div class='box'><img class='poster' src='images/poster_tour_ces_cru.jpg'></div>" +
-                           "<div class='box'><img class='poster' src='images/poster_show_jan_7.jpg'></div>" +
-                           "<div class='box'><img class='poster' src='images/poster_show_sep_23_2017.jpg'></div>");
+        $("#content").html(
+            "<div class='box'><a href='https://www.ticketweb.com/event/late-to-the-party-yj-sobs-tickets/8361045?pl=sobs&edpPlParam=%3Fpl%3Dsobs'><img class='poster' src='images/poster_show_jun_12_2018.jpg'></a><br/>June 12, 2018</div>" +
+            "<div class='box'><img class='poster' src='images/poster_tour_ces_cru.jpg'><br/>March 18, 2018</div>" +
+            "<div class='box'><img class='poster' src='images/poster_show_jan_7.jpg'><br/>January 7, 2018</div>" +
+            "<div class='box'><img class='poster' src='images/poster_show_sep_23_2017.jpg'><br/>September 23, 2017</div>");
     }
 }
 
