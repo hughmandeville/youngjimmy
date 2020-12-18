4d51dc4a9e664bd1f1fed06f7c9509ba1d135313
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2020-12-18 12:53:28 -0500

    Added

---
+++ shopify.js
@@ -0,0 +1,161 @@
+/*<![CDATA[*/
+(function () {
+  var scriptURL =
+    "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = scriptURL;
+    (
+      document.getElementsByTagName("head")[0] ||
+      document.getElementsByTagName("body")[0]
+    ).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: "hood-rock-records.myshopify.com",
+      storefrontAccessToken: "cb7c415588c189067d14346ef59a3103",
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent("collection", {
+        id: "237882900662",
+        node: document.getElementById("collection-component-1608208487968"),
+        moneyFormat: "%24%7B%7Bamount%7D%7D",
+        options: {
+          product: {
+            styles: {
+              product: {
+                "@media (min-width: 601px)": {
+                  "max-width": "calc(25% - 20px)",
+                  "margin-left": "20px",
+                  "margin-bottom": "50px",
+                  width: "calc(25% - 20px)",
+                },
+                img: {
+                  height: "calc(100% - 15px)",
+                  position: "absolute",
+                  left: "0",
+                  right: "0",
+                  top: "0",
+                },
+                imgWrapper: {
+                  "padding-top": "calc(75% + 15px)",
+                  position: "relative",
+                  height: "0",
+                },
+              },
+              title: {
+                color: "#faf4f4",
+              },
+              button: {
+                ":hover": {
+                  "background-color": "#e10505",
+                },
+                "background-color": "#fa0606",
+                ":focus": {
+                  "background-color": "#e10505",
+                },
+              },
+              price: {
+                color: "#faf6f6",
+              },
+              compareAt: {
+                color: "#faf6f6",
+              },
+              unitPrice: {
+                color: "#faf6f6",
+              },
+            },
+            buttonDestination: "modal",
+            contents: {
+              options: false,
+            },
+            text: {
+              button: "View product",
+            },
+          },
+          productSet: {
+            styles: {
+              products: {
+                "@media (min-width: 601px)": {
+                  "margin-left": "-20px",
+                },
+              },
+            },
+          },
+          modalProduct: {
+            contents: {
+              img: false,
+              imgWithCarousel: true,
+              button: false,
+              buttonWithQuantity: true,
+            },
+            styles: {
+              product: {
+                "@media (min-width: 601px)": {
+                  "max-width": "100%",
+                  "margin-left": "0px",
+                  "margin-bottom": "0px",
+                },
+              },
+              button: {
+                ":hover": {
+                  "background-color": "#e10505",
+                },
+                "background-color": "#fa0606",
+                ":focus": {
+                  "background-color": "#e10505",
+                },
+              },
+            },
+            text: {
+              button: "Add to cart",
+            },
+          },
+          cart: {
+            styles: {
+              button: {
+                ":hover": {
+                  "background-color": "#e10505",
+                },
+                "background-color": "#fa0606",
+                ":focus": {
+                  "background-color": "#e10505",
+                },
+              },
+            },
+            text: {
+              total: "Subtotal",
+              button: "Checkout",
+            },
+            popup: false,
+          },
+          toggle: {
+            styles: {
+              toggle: {
+                "background-color": "#fa0606",
+                ":hover": {
+                  "background-color": "#e10505",
+                },
+                ":focus": {
+                  "background-color": "#e10505",
+                },
+              },
+            },
+          },
+        },
+      });
+    });
+  }
+})();
+/*]]>*/
