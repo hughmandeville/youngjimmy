ace8e2a4380e6bc77f80abe6e1853f14c72025c8
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2023-02-19 20:48:29 -0500

    updated

--- shopify.js
+++ shopify.js
@@ -29,7 +29,7 @@
     ShopifyBuy.UI.onReady(client).then(function (ui) {
       ui.createComponent("collection", {
         id: "237882900662",
-        node: document.getElementById("collection-component-1608208487968"),
+        node: document.getElementById("collection-component-1676138676332"),
         moneyFormat: "%24%7B%7Bamount%7D%7D",
         options: {
           product: {
@@ -55,25 +55,28 @@
                 },
               },
               title: {
-                color: "#faf4f4",
+                color: "#060000",
               },
               button: {
+                "font-weight": "bold",
+                color: "#050000",
                 ":hover": {
-                  "background-color": "#e10505",
+                  color: "#050000",
+                  "background-color": "#d2e302",
                 },
-                "background-color": "#fa0606",
+                "background-color": "#e9fc02",
                 ":focus": {
-                  "background-color": "#e10505",
+                  "background-color": "#d2e302",
                 },
               },
               price: {
-                color: "#faf6f6",
+                color: "#080000",
               },
               compareAt: {
-                color: "#faf6f6",
+                color: "#080000",
               },
               unitPrice: {
-                color: "#faf6f6",
+                color: "#080000",
               },
             },
             buttonDestination: "modal",
@@ -109,29 +112,117 @@
                 },
               },
               button: {
+                "font-weight": "bold",
+                color: "#050000",
                 ":hover": {
-                  "background-color": "#e10505",
+                  color: "#050000",
+                  "background-color": "#d2e302",
                 },
-                "background-color": "#fa0606",
+                "background-color": "#e9fc02",
                 ":focus": {
-                  "background-color": "#e10505",
+                  "background-color": "#d2e302",
                 },
+              },
+              title: {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "bold",
+                "font-size": "26px",
+                color: "#060000",
+              },
+              price: {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "normal",
+                "font-size": "18px",
+                color: "#050000",
+              },
+              compareAt: {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "normal",
+                "font-size": "15.299999999999999px",
+                color: "#050000",
+              },
+              unitPrice: {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "normal",
+                "font-size": "15.299999999999999px",
+                color: "#050000",
+              },
+              description: {
+                color: "#050000",
               },
             },
             text: {
               button: "Add to cart",
             },
           },
+          option: {
+            styles: {
+              label: {
+                color: "#050000",
+              },
+            },
+          },
           cart: {
             styles: {
               button: {
+                "font-weight": "bold",
+                color: "#050000",
                 ":hover": {
-                  "background-color": "#e10505",
+                  color: "#050000",
+                  "background-color": "#d2e302",
                 },
-                "background-color": "#fa0606",
+                "background-color": "#e9fc02",
                 ":focus": {
-                  "background-color": "#e10505",
+                  "background-color": "#d2e302",
                 },
+              },
+              title: {
+                color: "#0a0000",
+              },
+              header: {
+                color: "#0a0000",
+              },
+              lineItems: {
+                color: "#0a0000",
+              },
+              subtotalText: {
+                color: "#0a0000",
+              },
+              subtotal: {
+                color: "#0a0000",
+              },
+              notice: {
+                color: "#0a0000",
+              },
+              currency: {
+                color: "#0a0000",
+              },
+              close: {
+                color: "#0a0000",
+                ":hover": {
+                  color: "#0a0000",
+                },
+              },
+              empty: {
+                color: "#0a0000",
+              },
+              noteDescription: {
+                color: "#0a0000",
+              },
+              discountText: {
+                color: "#0a0000",
+              },
+              discountIcon: {
+                fill: "#0a0000",
+              },
+              discountAmount: {
+                color: "#0a0000",
+              },
+              cart: {
+                "background-color": "#fffdfd",
+              },
+              footer: {
+                "background-color": "#fffdfd",
               },
             },
             text: {
@@ -143,13 +234,60 @@
           toggle: {
             styles: {
               toggle: {
-                "background-color": "#fa0606",
+                "font-weight": "bold",
+                "background-color": "#e9fc02",
                 ":hover": {
-                  "background-color": "#e10505",
+                  "background-color": "#d2e302",
                 },
                 ":focus": {
-                  "background-color": "#e10505",
+                  "background-color": "#d2e302",
                 },
+              },
+              count: {
+                color: "#050000",
+                ":hover": {
+                  color: "#050000",
+                },
+              },
+              iconPath: {
+                fill: "#050000",
+              },
+            },
+          },
+          lineItem: {
+            styles: {
+              variantTitle: {
+                color: "#0a0000",
+              },
+              title: {
+                color: "#0a0000",
+              },
+              price: {
+                color: "#0a0000",
+              },
+              fullPrice: {
+                color: "#0a0000",
+              },
+              discount: {
+                color: "#0a0000",
+              },
+              discountIcon: {
+                fill: "#0a0000",
+              },
+              quantity: {
+                color: "#0a0000",
+              },
+              quantityIncrement: {
+                color: "#0a0000",
+                "border-color": "#0a0000",
+              },
+              quantityDecrement: {
+                color: "#0a0000",
+                "border-color": "#0a0000",
+              },
+              quantityInput: {
+                color: "#0a0000",
+                "border-color": "#0a0000",
               },
             },
           },
