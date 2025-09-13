9d509c291b3f91def278a9ca89d2a77dfb156d9b
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2025-09-13 09:21:37 -0400

    fixed name

--- code.js
+++ code.js
@@ -9,7 +9,7 @@ var albums = [
     url: "",
   },
   {
-    front: "album_cover_rocket_88_new_animated.png",
+    front: "album_cover_rockett_88_new_animated.png",
     back: "album_back_rockett_88_new.jpg",
     title: "ROCKETT 88 (Deluxe)<br/>2023",
     alt: "ROCKETT 88",
